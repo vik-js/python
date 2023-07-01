85de62cdbfae9b794f55d853357ae939815a3353
viktoriya <emillia@bk.ru>
2023-07-02 02:12:45 +0500

    Решение задачи 2 с помощью интерполяции

--- HomeWork02.py
+++ HomeWork02.py
@@ -2,10 +2,13 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0) |
 
-print("Введте 3-х значное число:")
+print("Введите 3-х значное число:")
 num = int(input())
-digit_1 = num // 100
-digit_2 = num // 10 % 10
-digit_3 = num % 10
-sum = digit_1 + digit_2 + digit_3
-print(num, '->', sum, '(',digit_1, '+', digit_2, '+', digit_3,')')
+if (100 <= num < 1000):
+  digit_1 = num // 100
+  digit_2 = num // 10 % 10
+  digit_3 = num % 10
+  sum = digit_1 + digit_2 + digit_3
+  print(f'{num} -> {sum} ({digit_1} + {digit_2} + {digit_3})')
+else:
+  print('Введено НЕ 3-х значное число!')
